2760fb5923b166a1c50142da74cc6e6d14c0e883
QADRAX <carlos.delacuadra@devo.com>
2022-10-20 23:30:55 +0200

    use Log

--- server/firebaseAdmin.ts
+++ server/firebaseAdmin.ts
@@ -1,12 +1,14 @@
 import * as firebaseAdmin from "firebase-admin";
+import { Log } from "../utils/Log";
 
 const privateKey = process.env["PRIVATE_KEY"];
 const clientEmail = process.env["CLIENT_EMAIL"];
 const projectId = process.env["PROJECT_ID"];
 
 if (!privateKey || !clientEmail || !projectId) {
-  console.log(
-    `Failed to load Firebase credentials. Follow the instructions in the README to set your Firebase credentials inside environment variables.`
+  Log(
+    `Failed to load Firebase credentials. Follow the instructions in the README to set your Firebase credentials inside environment variables.`,
+    'critical'
   );
 }
 
